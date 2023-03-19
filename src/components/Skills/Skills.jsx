fcf9024e35e886b68a5ce061d0de1a89ae588697
Adi Narayana Reddy <adinarayana4444@gmail.com>
2023-03-20 01:17:11 +0530

    added animations

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -30,37 +30,37 @@ const Skills = () => {
 					<div className='blur4'></div>
 
 					<div className='skills-circle'>
-						<div className='html secCircle'>
+						<div className='html secCircle anim'>
 							<ImHtmlFive size={40} color='#E14E1D' />
 						</div>
-						<div className='css secCircle '>
+						<div className='css secCircle anim'>
 							<SiCss3 size={40} color='#0277BD' />
 						</div>
-						<div className='js secCircle'>
+						<div className='js secCircle anim'>
 							<SiJavascript size={40} color='#F0DB4F' />
 						</div>
-						<div className='react secCircle'>
+						<div className='react secCircle anim'>
 							<SiReact size={40} color='#00CDF2' />
 						</div>
-						<div className='redux secCircle'>
+						<div className='redux secCircle anim'>
 							<SiRedux size={40} color='#7046B2' />
 						</div>
-						<div className='bootstrap secCircle'>
+						<div className='bootstrap secCircle anim'>
 							<SiBootstrap size={40} color='#7311EC' />
 						</div>
-						<div className='sass secCircle'>
+						<div className='sass secCircle anim'>
 							<SiSass size={40} color='#CA689B' />
 						</div>
-						<div className='tailwind secCircle'>
+						<div className='tailwind secCircle anim'>
 							<SiTailwindcss size={40} color='#38BDF8' />
 						</div>
-						<div className='mui secCircle'>
+						<div className='mui secCircle anim'>
 							<SiMui size={40} color='#00A7F2' />
 						</div>
-						<div className='git secCircle'>
+						<div className='git secCircle anim'>
 							<FaGitAlt size={40} color='#F03C2E' />
 						</div>
-						<div className='github secCircle'>
+						<div className='github secCircle anim'>
 							<SiGithub size={40} color='#242938' />
 						</div>
 						<h2
